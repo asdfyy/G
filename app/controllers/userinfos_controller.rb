98fd23c13d2c14887aabf60ae64fbfd4893896df
asdfyy <58731446+asdfyy@users.noreply.github.com>
2020-05-28 19:00:16 +0900

    ユーザー情報表示OK（仮）

--- app/controllers/userinfos_controller.rb
+++ app/controllers/userinfos_controller.rb
@@ -1,6 +1,8 @@
 class UserinfosController < ApplicationController
   def show
     @user = current_user
-    @addresses = Address.all
+    @addresses = Address.where(user_id: current_user.id).first
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
-  get 'userinfos/show'
-
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
-#   devise_scope :user do
-#     get 'signup1', to: 'users/registrations#signup1'
-#     get 'signup2', to: 'users/registrations#signup2'
-#   end
 devise_scope :user do
   get 'addresses', to: 'users/registrations#new2'
   post 'addresses', to: 'users/registrations#create_address'
 end
+
+
+get 'userinfos/show'
 
   root 'toppage#index'
   get "/tyannko" => "toppage#tyannko"
